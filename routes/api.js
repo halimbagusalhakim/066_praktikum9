06e22c25e27568b95328a576cd55c193ea66249a
halimbagusalhakim <halsat132@gmail.com>
2025-11-24 10:36:03 +0700

    memperbaiki error

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,10 @@ const multer = require('multer');
 const komikcontroller = require('../controllers/komikcontroller');
 const upload = multer({storage: multer.memoryStorage()});
 
-router.post('/komik', upload.single('cover'), komikcontroller.createKomik);
+router.post('/komik', upload.single('gambar'), komikcontroller.createKomik);
 router.get('/komik', komikcontroller.getAllKomiks);
 router.get('/komik/:id', komikcontroller.getKomikById);
-router.put('/komik/:id', upload.single('cover'), komikcontroller.updateKomik);
+router.put('/komik/:id', upload.single('gambar'), komikcontroller.updateKomik);
 router.delete('/komik/:id', komikcontroller.deleteKomik);
 
 module.exports = router;
